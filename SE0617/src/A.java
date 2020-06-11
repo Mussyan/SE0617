9a9e57bd6115eac1439d61d0fb53a8a6ffc6980f
Mussyan <lost4712@gmail.com>
2020-06-11 14:02:49 +0900

    Fix class A

--- SE0617/src/A.java
+++ SE0617/src/A.java
@@ -3,7 +3,7 @@ public class A {
 	public static void main(String argv[]) {
 		B classB = new B();
 		C classC = new C();
-		System.out.println("This is Class A that version 1!");
+		System.out.println("This is Class A that version 2!");
 		classB.versionB();
 		classC.versionC();
 	}
